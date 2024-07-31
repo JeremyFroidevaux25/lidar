b3cb3dc878f7f3d1e4ffbc1b030ae3e17fe57dcf
JeremyFroidevauxCREA <jfroidevaux@creamontblanc.org>
2024-07-31 14:14:36 +0200

    description added

--- R/lidar_compar.R
+++ R/lidar_compar.R
@@ -1,9 +1,9 @@
 
 ##------------------------------------------------------------------------------------------------------------------
-## 1st part: extract mean, sd and cv of canopy height at sites monitored in the Mont-Blanc valley 
+## Part 1
+## extract mean, sd and cv of canopy height at sites monitored in the Mont-Blanc valley 
 ## (for both the grid points of the shrubland and the points of the vegetation survey)
 ##------------------------------------------------------------------------------------------------------------------
-
 
 library(terra)
 library(sf)
@@ -44,8 +44,7 @@ plot(chm2)
 plot(vegt,add=T)
 plot(landt,add=T)
 
-# Calculate mean and sd values of canopy height in a 10 x 10 m grid
-
+# Calculate mean and sd values of canopy height in a 20 x 20 m grid
 chm_recl1 <- chm1
 chm_recl1[chm_recl1 < 0] <- 0
 chm_recl1[chm_recl1 > 1.5] <- NA
@@ -63,25 +62,10 @@ chm_sd1<- aggregate(chm_recl1, fact = c(20, 20), fun = function(x) sd(x, na.rm =
 chm_mean2<- aggregate(chm_recl2, fact = c(20, 20), fun = function(x) mean(x, na.rm = TRUE))
 chm_sd2<- aggregate(chm_recl2, fact = c(20, 20), fun = function(x) sd(x, na.rm = TRUE))
 
-
+#Merge the two tiles
 chm_mean <- merge(chm_mean1, chm_mean2)
 chm_sd  <- merge(chm_sd1, chm_sd2)
 
-chm_meanR <- merge(chm_recl1, chm_recl2)
-plot(chm_meanR)
-terra::writeRaster(chm_meanR, "/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_1m.tif", overwrite=TRUE)
-
-
-terra::writeRaster(chm_mean, "/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_mean_20m_naomit.tif", overwrite=TRUE)
-terra::writeRaster(chm_sd, filename="/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_sd_20m_naomit.tif",  overwrite=TRUE)
-res <- res(chm_mean)
-res 
-
-
-plot(chm_mean)
-plot(vegt,add=T)
-plot(chm_sd)
-plot(vegt,add=T)
 
 # Extract values at point locations : veg shapefile
 extracted_values_mean <- extract(chm_mean,vegt)*100
@@ -105,92 +89,41 @@ land$CHMcv <- extracted_values_sd[,2]/extracted_values_mean[,2]*100
 landet<-as.data.frame(land)
 write.csv(landet,"/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_20m_naomit.csv")
 
+#Check the number of NAs
 nbna <- sum(is.na(landet$CHMmean))
 propna <- mean(is.na(landet$CHMmean))*100
 
+#Create and save a high-res CHM model (at 1m resolution)
+chm_meanR <- merge(chm_recl1, chm_recl2)
+plot(chm_meanR)
+terra::writeRaster(chm_meanR, "/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_1m.tif", overwrite=TRUE)
+
+terra::writeRaster(chm_mean, "/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_mean_20m_naomit.tif", overwrite=TRUE)
+terra::writeRaster(chm_sd, filename="/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_sd_20m_naomit.tif",  overwrite=TRUE)
+res <- res(chm_mean)
+res 
+
 ##------------------------------------------------------------------------------------------------------------------
-## 2nd part: Comparison of vegetation height (mean and sd) between LiDAR and vegetation survey
+## Part 2
+## Comparison of vegetation height (mean and sd) between LiDAR and vegetation survey
 ##------------------------------------------------------------------------------------------------------------------
+
 library(data.table)
+library(ggplot2)
+library(gridExtra)
+
 vegfield<-fread("/Users/jeremyfroidevaux/Library/CloudStorage/GoogleDrive-jfroidevaux@creamontblanc.org/Drive partagés/prototool/herbiland/analyses/landus/data/releve_vegetation/landus_20231109.csv")
-
-vegchm5<-fread("/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_5m.csv")
-names(vegchm5)[names(vegchm5) == "V2"] <- "plot_project"
-veg5<-merge(vegfield,vegchm5,by="plot_project")
-
-vegchm10<-fread("/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_10m.csv")
-names(vegchm10)[names(vegchm10) == "V2"] <- "plot_project"
-veg10<-merge(vegfield,vegchm10,by="plot_project")
-
-vegchm15<-fread("/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_15m.csv")
-names(vegchm15)[names(vegchm15) == "V2"] <- "plot_project"
-veg15<-merge(vegfield,vegchm15,by="plot_project")
 
 vegchm20<-fread("/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_20m.csv")
 names(vegchm20)[names(vegchm20) == "V2"] <- "plot_project"
 veg20<-merge(vegfield,vegchm20,by="plot_project")
-
-vegchm25<-fread("/Users/jeremyfroidevaux/Documents/Herbiland/Lidar/chm_25m.csv")
-names(vegchm25)[names(vegchm25) == "V2"] <- "plot_project"
-veg25<-merge(vegfield,vegchm25,by="plot_project")
 
 #Plot mean
 rsquared <- function(model) {
   summary(model)$r.squared
 }
 
-library(ggplot2)
-p5<-ggplot(veg5, aes(x = hcan, y = CHMmean)) +
-  geom_smooth(method = "lm", se = T,colour="grey") +
-  geom_point() + 
-  theme_minimal() +  # Set minimal theme
-  theme(
-    plot.background = element_rect(fill = "white"),  # Set background color to white
-    panel.background = element_rect(fill = "white"),  # Set panel background color to white
-    axis.line = element_line(color = "black"),  # Set axis line color to black
-    axis.text = element_text(color = "black"),  # Set axis text color to black
-    axis.title = element_text(color = "black")  # Set axis title color to black
-  ) +
-  labs(x = "Canopy height Field (cm)", y = "Canopy height Lidar (cm)", title = "Mean canopy height (5m)") + 
-  annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, 
-           label = paste("Slope:", round(coef(lm(CHMmean ~ hcan, data = veg5))[2],3),
-                         "R-squared:", round(rsquared(lm(CHMmean ~ hcan, data = veg5)), 3)))
-
-
-p10<-ggplot(veg10, aes(x = hcan, y = CHMmean)) +
-  geom_smooth(method = "lm", se = T,colour="grey") +
-  geom_point() + 
-  theme_minimal() +  # Set minimal theme
-  theme(
-    plot.background = element_rect(fill = "white"),  # Set background color to white
-    panel.background = element_rect(fill = "white"),  # Set panel background color to white
-    axis.line = element_line(color = "black"),  # Set axis line color to black
-    axis.text = element_text(color = "black"),  # Set axis text color to black
-    axis.title = element_text(color = "black")  # Set axis title color to black
-  ) +
-  labs(x = "Canopy height Field (cm)", y = "Canopy height Lidar (cm)", title = "Mean canopy height (10m)")  +
-  annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, 
-           label = paste("Slope:", round(coef(lm(CHMmean ~ hcan, data = veg10))[2], 3),
-                         "R-squared:", round(rsquared(lm(CHMmean ~ hcan, data = veg10)), 3)))
-
-
-p15<-ggplot(veg15, aes(x = hcan, y = CHMmean)) +
-  geom_smooth(method = "lm", se = T,colour="grey") +
-  geom_point() + 
-  theme_minimal() +  # Set minimal theme
-  theme(
-    plot.background = element_rect(fill = "white"),  # Set background color to white
-    panel.background = element_rect(fill = "white"),  # Set panel background color to white
-    axis.line = element_line(color = "black"),  # Set axis line color to black
-    axis.text = element_text(color = "black"),  # Set axis text color to black
-    axis.title = element_text(color = "black")  # Set axis title color to black
-  ) +
-  labs(x = "Canopy height Field (cm)", y = "Canopy height Lidar (cm)", title = "Mean canopy height (15m)")  +
-  annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, 
-           label = paste("Slope:", round(coef(lm(CHMmean ~ hcan, data = veg15))[2], 3),
-                         "R-squared:", round(rsquared(lm(CHMmean ~ hcan, data = veg15)), 3)))
-
-p20<-ggplot(veg20, aes(x = hcan, y = CHMmean)) +
+p20m<-ggplot(veg20, aes(x = hcan, y = CHMmean)) +
   geom_smooth(method = "lm", se = T,colour="grey") +
   geom_point() + 
   theme_minimal() +  # Set minimal theme
@@ -205,94 +138,14 @@ p20<-ggplot(veg20, aes(x = hcan, y = CHMmean)) +
   annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, 
            label = paste("Slope:", round(coef(lm(CHMmean ~ hcan, data = veg20))[2], 3),
                          "R-squared:", round(rsquared(lm(CHMmean ~ hcan, data = veg20)), 3)))
-
-p20
-
-
-p25<-ggplot(veg25, aes(x = hcan, y = CHMmean)) +
-  geom_smooth(method = "lm", se = T,colour="grey") +
-  geom_point() + 
-  theme_minimal() +  # Set minimal theme
-  theme(
-    plot.background = element_rect(fill = "white"),  # Set background color to white
-    panel.background = element_rect(fill = "white"),  # Set panel background color to white
-    axis.line = element_line(color = "black"),  # Set axis line color to black
-    axis.text = element_text(color = "black"),  # Set axis text color to black
-    axis.title = element_text(color = "black")  # Set axis title color to black
-  ) +
-  labs(x = "Canopy height Field (cm)", y = "Canopy height Lidar (cm)", title = "Mean canopy height (25m)")  +
-  annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, 
-           label = paste("Slope:", round(coef(lm(CHMmean ~ hcan, data = veg25))[2], 3),
-                         "R-squared:", round(rsquared(lm(CHMmean ~ hcan, data = veg25)), 3)))
-
-library(gridExtra)
-y_limits <- range(c(veg5$CHMmean, veg10$CHMmean, veg15$CHMmean), na.rm = TRUE)
-p5 <- p5 + ylim(y_limits)
-p10 <- p10 + ylim(y_limits)
-p15 <- p15 + ylim(y_limits)
-p20 <- p20 + ylim(y_limits)
-p25 <- p25 + ylim(y_limits)
-
-combined_plot <- grid.arrange(p5, p10, p15, p20,p25,ncol = 3)
-
+p20m
 
 #Plot sd
 rsquared <- function(model) {
   summary(model)$r.squared
 }
 
-library(ggplot2)
-p5<-ggplot(veg5, aes(x = heterog, y = CHMsd)) +
-  geom_smooth(method = "lm", se = T,colour="grey") +
-  geom_point() + 
-  theme_minimal() +  # Set minimal theme
-  theme(
-    plot.background = element_rect(fill = "white"),  # Set background color to white
-    panel.background = element_rect(fill = "white"),  # Set panel background color to white
-    axis.line = element_line(color = "black"),  # Set axis line color to black
-    axis.text = element_text(color = "black"),  # Set axis text color to black
-    axis.title = element_text(color = "black")  # Set axis title color to black
-  ) +
-  labs(x = "SD Canopy height Field (cm)", y = "SD Canopy height Lidar (cm)", title = "SD canopy height (5m)") + 
-  annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, 
-           label = paste("Slope:", round(coef(lm(CHMsd ~ heterog, data = veg5))[2],3),
-                         "R-squared:", round(rsquared(lm(CHMsd ~ heterog, data = veg5)), 3)))
-
-
-p10<-ggplot(veg10, aes(x = heterog, y = CHMsd)) +
-  geom_smooth(method = "lm", se = T,colour="grey") +
-  geom_point() + 
-  theme_minimal() +  # Set minimal theme
-  theme(
-    plot.background = element_rect(fill = "white"),  # Set background color to white
-    panel.background = element_rect(fill = "white"),  # Set panel background color to white
-    axis.line = element_line(color = "black"),  # Set axis line color to black
-    axis.text = element_text(color = "black"),  # Set axis text color to black
-    axis.title = element_text(color = "black")  # Set axis title color to black
-  ) +
-  labs(x = "SD Canopy height Field (cm)", y = "SD Canopy height Lidar (cm)", title = "SD canopy height (10m)")  +
-  annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, 
-           label = paste("Slope:", round(coef(lm(CHMsd ~ heterog, data = veg10))[2], 3),
-                         "R-squared:", round(rsquared(lm(CHMsd ~ heterog, data = veg10)), 3)))
-
-
-p15<-ggplot(veg15, aes(x = heterog, y = CHMsd)) +
-  geom_smooth(method = "lm", se = T,colour="grey") +
-  geom_point() + 
-  theme_minimal() +  # Set minimal theme
-  theme(
-    plot.background = element_rect(fill = "white"),  # Set background color to white
-    panel.background = element_rect(fill = "white"),  # Set panel background color to white
-    axis.line = element_line(color = "black"),  # Set axis line color to black
-    axis.text = element_text(color = "black"),  # Set axis text color to black
-    axis.title = element_text(color = "black")  # Set axis title color to black
-  ) +
-  labs(x = "SD Canopy height Field (cm)", y = "SD Canopy height Lidar (cm)", title = "SD canopy height (15m)")  +
-  annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, 
-           label = paste("Slope:", round(coef(lm(CHMsd ~ heterog, data = veg15))[2], 3),
-                         "R-squared:", round(rsquared(lm(CHMsd ~ heterog, data = veg15)), 3)))
-
-p20<-ggplot(veg20, aes(x = heterog, y = CHMsd)) +
+p20s<-ggplot(veg20, aes(x = heterog, y = CHMsd)) +
   geom_smooth(method = "lm", se = T,colour="grey") +
   geom_point() + 
   theme_minimal() +  # Set minimal theme
@@ -307,16 +160,13 @@ p20<-ggplot(veg20, aes(x = heterog, y = CHMsd)) +
   annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, 
            label = paste("Slope:", round(coef(lm(CHMsd ~ heterog, data = veg20))[2], 3),
                          "R-squared:", round(rsquared(lm(CHMsd ~ heterog, data = veg20)), 3)))
+p20s
 
-library(gridExtra)
-y_limits <- range(c(veg5$CHMmean, veg10$CHMmean, veg15$CHMmean), na.rm = TRUE)
-p5 <- p5 + ylim(y_limits)
-p10 <- p10 + ylim(y_limits)
-p15 <- p15 + ylim(y_limits)
-p20 <- p20 + ylim(y_limits)
-combined_plot <- grid.arrange(p5, p10, p15, p20,ncol = 2)
+#Merge plots
+combined_plot <- grid.arrange(p20m,p20s,ncol = 2)
+combined_plot
 
-### Test with a linear regression
+# Test with a linear regression
 M1<-lm(log(veg20$hcan+1)~veg20$CHMmean)
 summary(M1)
 shapiro.test(residuals(M1))
